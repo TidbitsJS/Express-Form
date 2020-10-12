b3ed607a60c377f382afbaa2c730c9e7c87d0b6d
Sujata Gunale <tidbitsjs@gmail.com>
2020-10-12 18:22:48 +0530

    register post

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require("express");
+const bcrypt = require("bcrypt");
 const app = express();
+
+const users = [];
 
 app.set("view-engine", "ejs");
 app.use(express.static(__dirname + "/public"));
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
   res.render("index.ejs", { message: "Tidbits" });
@@ -12,8 +16,27 @@ app.get("/login", (req, res) => {
   res.render("login.ejs");
 });
 
+app.post("/login", (req, res) => {});
+
 app.get("/register", (req, res) => {
   res.render("register.ejs");
+});
+
+app.post("/register", async (req, res) => {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    users.push({
+      id: Date.now().toString(),
+      name: req.body.name,
+      email: req.body.email,
+      password: hashedPassword,
+    });
+    res.redirect("/login");
+  } catch (e) {
+    res.redirect("/register");
+  }
+
+  console.log(users);
 });
 
 app.listen(3000);
