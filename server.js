f76c87b459a5e624ee61d5868427ef5a2cf3407e
Sujata Gunale <tidbitsjs@gmail.com>
2020-10-12 17:06:56 +0530

    register page

--- server.js
+++ server.js
@@ -2,9 +2,18 @@ const express = require("express");
 const app = express();
 
 app.set("view-engine", "ejs");
+app.use(express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => {
   res.render("index.ejs", { message: "Tidbits" });
+});
+
+app.get("/login", (req, res) => {
+  res.render("login.ejs");
+});
+
+app.get("/register", (req, res) => {
+  res.render("register.ejs");
 });
 
 app.listen(3000);
